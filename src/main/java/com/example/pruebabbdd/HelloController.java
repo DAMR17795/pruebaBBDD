2dbf6010baaf19d56e76599167c3d6149e64bcd7
DANIEL2DAM <dmarrom577@g.educaand.es>
2022-11-03 07:53:03 +0100

    INSTIUTO

--- src/main/java/com/example/pruebabbdd/HelloController.java
+++ src/main/java/com/example/pruebabbdd/HelloController.java
@@ -5,8 +5,12 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.util.Callback;
 
 import java.sql.Connection;
@@ -24,17 +28,16 @@ public class HelloController {
 
     @Deprecated
     public void onEjecutarConsulta(ActionEvent actionEvent) {
-       /* Dynamic TableView Data From Database
-          BY Narayan G. Maharjan https://blog.ngopal.com.np/2011/10/19/dyanmic-tableview-data-from-database/comment-page-1/
-          Modificado por FDGA
-        */
+     obtenerProductos();
+    }
+
+    public void cargarDatos2() {
         Connection c;
         data = FXCollections.observableArrayList();
         try {
             c = DriverManager.getConnection("jdbc:mariadb://localhost:5555/noinch?useSSL=false"
                     ,"adminer",
                     "adminer");;
-
 
             // Borramos por si es una nueva consulta
             if (! tvDatos.getColumns().isEmpty()) {
@@ -46,7 +49,8 @@ public class HelloController {
 
 
             //SQL FOR SELECTING ALL OF CUSTOMER
-            String SQL = txtAreaConsulta.getText();
+            //String SQL = txtAreaConsulta.getText();
+            String SQL = "SELECT ProductName FROM Products";
             //ResultSet
             ResultSet rs = c.createStatement().executeQuery(SQL);
 
@@ -96,5 +100,60 @@ public class HelloController {
             System.out.println("Error on Building Data");
         }
     }
+    private Connection conexionBBDD;
+    private final String servidor = "jdbc:mariadb://localhost:5555/noinch?useSSL=false";
+    private final String usuario = "adminer";
+    private final String passwd = "adminer";
 
+
+        public ObservableList<Productos> obtenerProductos() {
+
+            ObservableList<Productos> datosResultadoConsulta = FXCollections.observableArrayList();
+            try {
+                // Nos conectamos
+                conexionBBDD = DriverManager.getConnection(servidor, usuario, passwd);
+                String SQL = "SELECT * "
+                        + "FROM products "
+                        + "ORDER By productName";
+
+                // Ejecutamos la consulta y nos devuelve una matriz de filas (registros) y columnas (datos)
+                ResultSet resultadoConsulta = conexionBBDD.createStatement().executeQuery(SQL);
+
+                // Debemos cargar los datos en el ObservableList (Que es un ArrayList especial)
+                // Los datos que devuelve la consulta no son directamente cargables en nuestro objeto
+                while (resultadoConsulta.next()) {
+                    datosResultadoConsulta.add(new Productos(
+                            resultadoConsulta.getString("productCode"),
+                            resultadoConsulta.getString("productName"),
+                            resultadoConsulta.getString("productLine"),
+                            resultadoConsulta.getString("productScale"),
+                            resultadoConsulta.getString("productVendor"),
+                            resultadoConsulta.getString("productDescription"),
+                            resultadoConsulta.getInt("quantityInStock"),
+                            resultadoConsulta.getDouble("buyPrice"),
+                            resultadoConsulta.getDouble("MSRP"))
+                    );
+                    System.out.println("Row [1] added " + resultadoConsulta.toString());
+                }
+                conexionBBDD.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Error:" + e.toString());
+                conexionBBDD.close();
+            } finally {
+                return datosResultadoConsulta;
+            }
+        }
+
+    @FXML
+    public void pulsarIntro(Event event) {
+        txtAreaConsulta.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+                    obtenerProductos();
+                }
+            }
+        });
+    }
 }
